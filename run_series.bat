f0f693a2563d64e1309c9c56a949b7e34ddce6c8
danyalziakhan <danyal6870@gmail.com>
2025-03-20 22:34:22 +0500

    Change license year to 2025

--- run_series.bat
+++ run_series.bat
@@ -1,1 +1,1 @@
-.venv\Scripts\python.exe main.py --surface_width 31 --surface_height 16 --throw_ratio 0.8 --distance_series "15-20" --aspect_ratio "16:9" --output_dir output
+.venv\Scripts\python.exe main.py --surface_width 31 --surface_height 16 --throw_ratio 0.8 --distance_series "15-20" --aspect_ratio "16:10" --output_dir output
